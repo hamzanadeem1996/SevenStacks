ba30c5da237830f6e950a41d197d73181758444f
Tooba Akbar <toobaakbar96@gmail.com>
2022-07-29 04:30:26 +0500

    code optimization and complete footer

---
+++ src/components/ServicesArray.js
@@ -0,0 +1,47 @@
+import {
+  DedicatedTeamIcon,
+  SofwareDevIcon,
+  ServiceUIIcon,
+  SalesForceIcon,
+  DataScienceIcon,
+  DiscoveryWorkshopIcon,
+} from "./SvgIcons";
+
+export const ServicesArray = [
+  {
+    href: "/",
+    title: "Dedicated Teams",
+    desc: "We grow your team using our development resources and recruitment capabilities.",
+    icon: <DedicatedTeamIcon />,
+  },
+  {
+    href: "/",
+    title: "Custom Software Development",
+    desc: "We work with startups, SMBs & world leading enterprises to build customized software solutions.",
+    icon: <SofwareDevIcon />,
+  },
+  {
+    href: "/",
+    title: "UI/UX Design",
+    desc: "Our team of experienced product designers & developers help you build successful digital products. ",
+    icon: <ServiceUIIcon />,
+  },
+  {
+    href: "/",
+    title: "Salesforce",
+    desc: "We help you develop robust and reliable Salesforce Solutions to surpass your business expectations.",
+    icon: <SalesForceIcon />,
+  },
+  {
+    href: "/",
+    title: "Data Science",
+    desc: "We help organizations adopt data driven insights and automated reporting for faster decision making.",
+    icon: <DataScienceIcon />,
+  },
+  {
+    href: "/",
+    title: "Discovery Workshop",
+    desc: "We help you define your idea from business perspectives through our 2 weeks collaborative discovery workshop.",
+    icon: <DiscoveryWorkshopIcon />,
+  },
+];
--- src/components/index.js
+++ src/components/index.js
@@ -1,22 +1,12 @@
 import React from "react";
-
 import banner from "../static/images/banner-img.gif";
+import { ServicesArray } from "./ServicesArray";
 import CustomerSlider from "./CustomerSlider";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import Process from "./Process";
 
-import {
-  DedicatedTeamIcon,
-  SofwareDevIcon,
-  ServiceUIIcon,
-  SalesForceIcon,
-  DataScienceIcon,
-  DiscoveryWorkshopIcon,
-} from "./SvgIcons";
-
 const LandingPage = () => {
-
   const ServiceCard = ({ title, description, href, children }) => {
     return (
       <a href={href} className="service-card">
@@ -31,8 +21,8 @@ const LandingPage = () => {
     <>
       <Navbar />
 
-      <header className="masthead" style={{ backgroundColor: "#fff " }}>
-        <div className="container" style={{ marginLeft: "4.5rem" }}>
+      <header className="masthead">
+        <div className="container header-align">
           <div className="row">
             <div className="col-md-6 masthead-heading text-uppercase">
               Quality over Quantity
@@ -49,48 +39,20 @@ const LandingPage = () => {
         </div>
       </header>
       <div className="wrapper">
-        {" "}
         <section className="page-section" id="services">
           <h2 className="section-heading ">Services We Offer</h2>
           <div className="container feature" data-aos="fade-up">
             <div className="row card-grid">
-              <ServiceCard
-                href="/"
-                title="Dedicated Teams"
-                description="We grow your team using our development resources and recruitment capabilities."
-              >
-                <DedicatedTeamIcon />
-              </ServiceCard>
-              <ServiceCard
-                title="Custom Software Development"
-                description="We work with startups, SMBs & world leading enterprises to build customized software solutions."
-              >
-                <SofwareDevIcon />
-              </ServiceCard>
-              <ServiceCard
-                title="UI/UX Design"
-                description="Our team of experienced product designers & developers help you build successful digital products. "
-              >
-                <ServiceUIIcon />
-              </ServiceCard>
-              <ServiceCard
-                title="Salesforce"
-                description="We help you develop robust and reliable Salesforce Solutions to surpass your business expectations. "
-              >
-                <SalesForceIcon />
-              </ServiceCard>
-              <ServiceCard
-                title="Data Science"
-                description="We help organizations adopt data driven insights and automated reporting for faster decision making. "
-              >
-                <DataScienceIcon />
-              </ServiceCard>
-              <ServiceCard
-                title="Discovery Workshop"
-                description="We help you define your idea from business perspectives through our 2 weeks collaborative discovery workshop."
-              >
-                <DiscoveryWorkshopIcon />
-              </ServiceCard>
+              {ServicesArray.map((item, index) => (
+                <ServiceCard
+                  key={index}
+                  href={item.link}
+                  title={item.title}
+                  description={item.desc}
+                >
+                  {item.icon}
+                </ServiceCard>
+              ))}
             </div>
           </div>
         </section>
@@ -110,25 +72,6 @@ const LandingPage = () => {
       </div>
 
       <Footer />
-
-      {/* <footer className="footer py-4" style={{ backgroundColor: "#48aceb" }}>
-        <div className="container">
-          <div className="row align-items-center">
-            <div className="col-lg-4 text-lg-left" style={{ color: "white" }}>
-              Copyright © Tweesent 2020
-            </div>
-            <div className="col-lg-4 my-3 my-lg-0"></div>
-            <div className="col-lg-4 text-lg-right">
-              <a className="mr-3" href="/contact" style={{ color: "white" }}>
-                Send Email
-              </a>
-              <a href="#about" style={{ color: "white" }}>
-                About Us
-              </a>
-            </div>
-          </div>
-        </div>
-      </footer> */}
     </>
   );
 };
